76787b8469fb664f2ad24125a086acf6d6908159
brucexhe <415988870@qq.com>
2024-04-30 09:48:55 +0800

    xxx

---
+++ Dboard/Extensions/ObjectExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Dboard
+{
+    public static class ObjectExtension
+    {
+        static JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        public static string ToJson(this Object obj)
+        {
+            if (obj == null)
+            {
+                return "null";
+            }
+            return JsonSerializer.Serialize(obj, options);
+        }
+    }
+}
