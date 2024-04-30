f6f61d49477b8faa50b1e14554941612caed2850
brucexhe <415988870@qq.com>
2024-04-30 16:24:49 +0800

    增加类R

---
+++ Dboard/Handlers/AutowiredAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Dboard
+{
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = false)]
+    public class AutowiredAttribute : Attribute
+    {
+    }
+}
---
+++ Dboard/Models/R.cs
@@ -0,0 +1,29 @@
+﻿namespace Dboard
+{
+    public class R
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public object data { get; set; }
+
+
+        public static R Success(object data, string message = "success")
+        {
+            R r = new R();
+            r.code = 200;
+            r.data = data;
+            r.message = message;
+
+            return r;
+        }
+
+        public static R Error(string message = "error", int code = 500)
+        {
+            R r = new R();
+            r.code = code;
+            r.message = message;
+
+            return r;
+        }
+    }
+}
