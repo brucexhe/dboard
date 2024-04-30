1ae0d628aae775102151ac9abedd3cf7e7415185
brucexhe <415988870@qq.com>
2024-04-30 10:29:23 +0800

    add SqliteDbContext

---
+++ Dboard/Db/SqliteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dboard.Db
+{
+    public class SqliteDbContext : DbContext
+    { 
+
+        public SqliteDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Dboard/Db/User.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dboard.Db
+{
+
+    [Table("user")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+        public int Status { get; set; }
+
+        public User() { }
+
+    }
+}
