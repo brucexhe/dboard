701e641aac55e5257cd59a9ea4f5aee3db686d42
brucexhe <415988870@qq.com>
2024-05-02 17:03:21 +0800

    add report

---
+++ Dboard/Handlers/ProgressReporter.cs
@@ -0,0 +1,15 @@
+﻿using Docker.DotNet.Models;
+using log4net;
+
+namespace Dboard.Handlers
+{
+    public class ProgressReporter : IProgress<JSONMessage>
+    {
+        protected ILog log = LogManager.GetLogger("ProgressReporter");
+
+        public void Report(JSONMessage value)
+        {
+            log.Info(value.ErrorMessage != null ? value.ErrorMessage : value.Status);
+        }
+    }
+}
